30e37de25848c35367be1c65d12f4fc7e1395fdd
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-11-04 04:55:21 +0200

    partialy complete the nav bar & add favicon

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import AboutUs from "./Pages/About Us/AboutUs";
 export const routes = createBrowserRouter([
   {
     path: "",
     element: <App />,
-    children: [],
+    children: [
+      {
+        path: "/aboutUs",
+        element: <AboutUs />,
+      },
+    ],
   },
 ]);
--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Style/navBar.css";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../Assets/logos/لوجو2.png";
+
 const NavBar = () => {
-  return <>
-  
-  </>;
+  const [navScroll, setNavScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= (10 * window.innerHeight) / 100) {
+        setNavScroll(true);
+      } else {
+        setNavScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navClass = navScroll ? "nav-scroll" : "";
+
+  return (
+    <>
+      <header>
+        <nav className={navClass}>
+          <div className="container">
+            <Link to={"/"} className="logo-link">
+              <img src={logo} alt="logo" />
+            </Link>
+            <div className="nav-links">
+              <ul className="links">
+                <li className="link">
+                  <NavLink to={"/"}>الرئيسية</NavLink>
+                </li>
+                <li className="link">
+                  <NavLink to={"cat"}>الأقسام</NavLink>
+                </li>
+                <li className="link">
+                  <NavLink to={"/aboutUs"}>من نحن</NavLink>
+                </li>
+                <li className="link">
+                  <NavLink to={"contactUs"}>تواصل معنا</NavLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className="nav-btns">
+              <Link to={""} className="main-btn">
+                تسجيل الدخول
+              </Link>
+              <Link to={""} className="main-btn second-btn">
+                الأشتراك
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default NavBar;
