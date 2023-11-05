bc0493c3329db6911862ea4c4b8538d5c250bd23
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-11-05 02:33:55 +0200

    finish nav bar in large screens

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../Style/navBar.css";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../Assets/logos/لوجو2.png";
-
+import { AiFillCaretDown } from "react-icons/ai";
 const NavBar = () => {
   const [navScroll, setNavScroll] = useState(false);
 
@@ -25,42 +25,61 @@ const NavBar = () => {
   const navClass = navScroll ? "nav-scroll" : "";
 
   return (
-    <>
-      <header>
-        <nav className={navClass}>
-          <div className="container">
-            <Link to={"/"} className="logo-link">
-              <img src={logo} alt="logo" />
-            </Link>
-            <div className="nav-links">
-              <ul className="links">
-                <li className="link">
+    <header>
+      <nav className={navClass}>
+        <div className="container">
+          <Link to={"/"} className="logo-link">
+            <img src={logo} alt="logo" />
+          </Link>
+          <div className="nav-links">
+            <ul className="links">
+              <li className="link">
+                <div className="nav-link">
                   <NavLink to={"/"}>الرئيسية</NavLink>
-                </li>
-                <li className="link">
-                  <NavLink to={"cat"}>الأقسام</NavLink>
-                </li>
-                <li className="link">
+                </div>
+              </li>
+              <li className="link menu">
+                <span>
+                  <AiFillCaretDown /> الأقسام
+                </span>
+                <div className="drop-down">
+                  <ul className="drop-down-list">
+                    <li className="drop-link">
+                      <NavLink to={""}>القرآن الكريم</NavLink>
+                    </li>
+                    <li className="drop-link">
+                      <NavLink to={""}>الراديو</NavLink>
+                    </li>
+                    <li className="drop-link">
+                      <NavLink to={""}>بث مباشر</NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="link">
+                <div className="nav-link">
                   <NavLink to={"/aboutUs"}>من نحن</NavLink>
-                </li>
-                <li className="link">
+                </div>
+              </li>
+              <li className="link">
+                <div className="nav-link">
                   <NavLink to={"contactUs"}>تواصل معنا</NavLink>
-                </li>
-              </ul>
-            </div>
-
-            <div className="nav-btns">
-              <Link to={""} className="main-btn">
-                تسجيل الدخول
-              </Link>
-              <Link to={""} className="main-btn second-btn">
-                الأشتراك
-              </Link>
-            </div>
+                </div>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </header>
-    </>
+
+          <div className="nav-btns">
+            <Link to={""} className="main-btn">
+              تسجيل الدخول
+            </Link>
+            <Link to={""} className="main-btn second-btn">
+              الأشتراك
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
