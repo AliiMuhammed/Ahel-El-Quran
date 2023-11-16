4101d88843f6133471f2363bf638588655b45bb2
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-11-16 04:06:38 +0200

    footer changes
    Refactor and enhance Footer component layout
    
    This commit refactors and enhances the layout of the Footer component by
    adding a container with three columns.

--- src/Shared/components/Footer.jsx
+++ src/Shared/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "../Style/footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="col-1"></div>
+        <div className="col-2"></div>
+        <div className="col-3"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
