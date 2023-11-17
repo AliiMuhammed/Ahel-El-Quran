d1047aa9b1c9d25e2112ddccd1b915e8aea8bd58
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-11-17 19:19:39 +0200

    Add MovetoTop component for scrolling to the top of the page.
    In this commit, a new component called MovetoTop was added to the App.js
    file. This component allows users to easily scroll to the top of the
    page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./Shared/components/NavBar";
 import Footer from "./Shared/components/Footer";
+import MovetoTop from "./Shared/components/MovetoTop";
 
 function App() {
   return (
     <>
+      <MovetoTop />
       <NavBar />
       <Outlet />
       <Footer />
