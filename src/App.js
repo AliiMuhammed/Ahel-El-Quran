7d488a0db2ff48bd2f7cc46270ad2d8cbc047876
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-11-18 06:51:11 +0200

    feat: Add react-scroll-to-top package
    This commit adds the React package "react-scroll-to-top" to the
    project's dependencies. This package allows for smooth scrolling to the
    top of the page. It is configured with a class name, width, and color.
    The new package is also imported and used in the App component for the
    scroll-to-top functionality, which is triggered when clicking a button
    with the "scrollToTop-btn" class. Additionally, CSS styles are added to
    the index.css file to customize the appearance of the scroll-to-top
    button, including transitions and hover effects.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./Shared/components/NavBar";
 import Footer from "./Shared/components/Footer";
 import MovetoTop from "./Shared/components/MovetoTop";
-
+import ScrollToTop from "react-scroll-to-top";
 function App() {
   return (
     <>
@@ -10,6 +10,12 @@ function App() {
       <NavBar />
       <Outlet />
       <Footer />
+      <ScrollToTop
+        smooth
+        className="scrollToTop-btn"
+        width="25px"
+        color="#fff"
+      />
     </>
   );
 }
