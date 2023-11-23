d7d94e7c592174bc9ce0968adc2237ee444ff51d
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-11-23 13:24:51 +0200

    feat: Enhance reader functionality and UX
    Improve the functionality and user experience of the reader feature.
    - Simplify the surah selection process by introducing an interactive
    button grid.
    - Update the initial selected surah and its corresponding class for
    highlighting.
    - Add audio player functionality for playing selected surahs.
    - Resize and position elements for better responsiveness on different
    devices.
    - Update the main heading to correctly display the reader's name.
    - Resolve issues related to CSS styles.
    
    These changes enhance the usability of the reader by providing a more
    intuitive way to select and play surahs, improving the overall user
    experience.
    
    Fixes #123

--- src/Pages/Single Reader/Reader.jsx
+++ src/Pages/Single Reader/Reader.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import MainHeading from "../../Shared/components/MainHeading";
 import Form from "react-bootstrap/Form";
-
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 import "./style/reader.css";
 
 const Reader = () => {
   const reader = useSelector((state) => state.readerData);
-  const [selectedSurahIndex, setSelectedSurahIndex] = useState(0); // State to track selected Surah index
-
+  const [selectedRwayaIndex, setSelectedRwayaIndex] = useState(0);
+  const [selectedSurahIndex, setSelectedSurahIndex] = useState("001");
+  const [selectedSurahClass, setSelectedSurahClass] = useState("");
   if (!reader) {
     return <section>Reader details not found.</section>;
   }
@@ -137,23 +139,42 @@ const Reader = () => {
   };
 
   const handleSurahSelect = (e) => {
-    setSelectedSurahIndex(parseInt(e.target.value, 10)); // Update selected Surah index
+    setSelectedRwayaIndex(parseInt(e.target.value, 10)); // Update selected Surah index
   };
-  console.log(reader);
 
-  let surahs_numbers = reader.moshaf[selectedSurahIndex].surah_list
+  let surahs_numbers = reader.moshaf[selectedRwayaIndex].surah_list
     .split(",")
-    .map(Number);
+    .map(Number)
+    .map((num) => num.toString().padStart(3, "0"));
+
+  const handleSurahClick = (number) => {
+    setSelectedSurahIndex(number);
+
+    setSelectedSurahClass(`second-btn-${number}`);
+  };
   const surahElements = surahs_numbers.map((number) => (
-    <div className="surah" key={number}>
-      {surahNamesArabic[number - 1]}{" "}
-      {/* Adjust index since surahNumbers start from 1 */}
+    <div
+      className={`surah main-btn ${
+        selectedSurahClass === `second-btn-${number}` ? "second-btn" : ""
+      }`}
+      key={number}
+      onClick={() => handleSurahClick(number)}
+    >
+      {`${number} - ${surahNamesArabic[number - 1]}`}
     </div>
   ));
-  console.log(surahs_numbers);
+  console.log(reader.moshaf[selectedRwayaIndex]);
+
+  const audioElement = () => {
+    return (
+      <AudioPlayer
+        src={`${reader.moshaf[selectedRwayaIndex].server}/${selectedSurahIndex}.mp3`}
+      />
+    );
+  };
   return (
     <section className="reader-section">
-      <MainHeading title={`القارء ${reader.name}`} breadcrumb={breadcrumb} />
+      <MainHeading title={`القارئ ${reader.name}`} breadcrumb={breadcrumb} />
       <section className="reader-content">
         <div className="container">
           <Form.Select
@@ -170,8 +191,9 @@ const Reader = () => {
               );
             })}
           </Form.Select>
+          <div className="surahs">{surahElements}</div>
+          <div className="surah-audio">{audioElement()}</div>
         </div>
-        <div className="surahs">{surahElements}</div>
       </section>
     </section>
   );
