c93fac5958bc41ad23e785aa9cd6f82d99b089ef
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-11-25 23:44:06 +0200

    Refactor contact us page layout to improve user experience and add localization support
    The contact us page layout has been refactored to enhance user
    experience. The changes include restructuring the page, adding a
    breadcrumb navigation, and adjusting the position of the contact form
    and map. Additionally, the page now supports localization by displaying
    text in Arabic. These improvements will help users easily navigate the
    page and interact with the contact form. No issues related to this
    change.
    
    Note: The commit message assumes that the changes were made to improve
    user experience and add localization support based on the provided code
    diff and user context.

--- src/Pages/Contact Us/ContactUs.jsx
+++ src/Pages/Contact Us/ContactUs.jsx
@@ -1,7 +1,65 @@
 import React from "react";
 import "./style/contactUs.css";
+import MainHeading from "./../../Shared/components/MainHeading";
+import MainHeader from "./../../Shared/components/MainHeader";
+import { Link } from "react-router-dom";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  const breadcrumb = {
+    الرئيسية: "/",
+    تواصل: "/contactUs",
+  };
+  return (
+    <section className="contact-section">
+      <MainHeading title="تواصل معنا" breadcrumb={breadcrumb} />
+      <section className="contact-us-form">
+        <div className="container">
+          <MainHeader
+            Header={"لََا تَتَردَّد فِي تَرْك رِسالة"}
+            smHeader={"دَعنَا نَتَواصَل"}
+          />
+          <div className="content">
+            <div className="right">
+              <div className="right-links">
+                <Link to={""}>
+                  <span>
+                    <FaLocationDot />
+                  </span>
+                  القاهرة - مصر
+                </Link>
+                <Link to={"mailto:ali.muhammed.dev@gmail.com"}>
+                  <span>
+                    <IoMail />
+                  </span>
+                  ali.muhammed.dev@gmail.com
+                </Link>
+              </div>
+              <div className="right-map">
+                <iframe
+                  title="helwan map"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d221095.76596880506!2d31.137063191685275!3d30.019027319177756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458368becfaa68d%3A0xb8a6f7d9576f81f8!2z2YPZhNmK2Kkg2KfZhNit2KfYs9io2KfYqiDZiNin2YTYsNmD2KfYoSDYp9mE2KfYtdi32YbYp9i52YrYjCDYrNin2YXYudipINit2YTZiNin2YY!5e0!3m2!1sen!2seg!4v1700944230513!5m2!1sen!2seg"
+                  style={{ border: 0 }}
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
+            </div>
+            <div className="left">
+              <form action="">
+                <input type="text" placeholder="الاسم" />
+                <input type="text" placeholder="البريد الإلكتروني" />
+                <textarea type="text" placeholder="رسالتك" />
+                <input type="submit" value="إرسال" className="main-btn" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default ContactUs;
