125060cd3672f7f3cf927784b6c804141b29fbff
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-12-07 20:19:27 +0200

    Fix incorrect property name in fetchLocation action
    Correct a typo in the code that caused the state value to not be
    assigned correctly. The typo for the 'state' property is fixed, ensuring
    the correct value is now assigned. This resolves an issue where the
    state value was not being retrieved properly from the received address
    data. No potential side effects are expected from this fix.
    
    Fixes #1234

--- src/Redux/Actions/Location.js
+++ src/Redux/Actions/Location.js
@@ -35,7 +35,7 @@ export const fetchLocation = () => {
                 console.log("Full address data:", data);
 
                 const city =
-                  data.address.statew ||
+                  data.address.state ||
                   data.address.town ||
                   data.address.village ||
                   data.address.county ||
