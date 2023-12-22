a8cd4cc383724ac8290f35a3864a8e481c97fd6a
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-12-22 14:45:31 +0200

    Refactor login page UI and structure
    Refactor the login page to enhance user experience with a new responsive
    layout and styling. Updated the form and background image. Improves
    readability and aesthetic appeal.

--- src/Pages/Authentaction/Login/Login.jsx
+++ src/Pages/Authentaction/Login/Login.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./style/login.css";
+import Form from "react-bootstrap/Form";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="login-section">
+      <div className="container">
+        <div className="form">
+          <Form.Control type="email" placeholder="البريد الإلكتروني" />
+          <Form.Control type="password" placeholder="كلمة السر" />
+          <button className="main-btn" type="submit">
+            تسجيل الدخول
+          </button>
+        </div>
+        <div className="img-login"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
