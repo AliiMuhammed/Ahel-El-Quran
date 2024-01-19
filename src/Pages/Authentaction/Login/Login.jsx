7888f966a6bcd5a26f923e8be013364bd20bb3aa
AliiMuhammed <mohamedtaha8000@gmail.com>
2024-01-19 23:38:28 +0200

    Refactor login and sign-up forms for Arabic support
    Convert login and sign-up forms to support Arabic language, update form
    labels and placeholders for improved user experience. Modify layout for
    better aesthetics and responsiveness. Add conditional rendering to
    sign-up form for future expansion. No associated issues.

--- src/Pages/Authentaction/Login/Login.jsx
+++ src/Pages/Authentaction/Login/Login.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import "./style/login.css";
 import Form from "react-bootstrap/Form";
+import { MdEmail } from "react-icons/md";
+import { FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
     <section className="login-section">
       <div className="container">
         <div className="form">
-          <Form.Control type="email" placeholder="البريد الإلكتروني" />
-          <Form.Control type="password" placeholder="كلمة السر" />
+          <div className="form-title">تسجيل الدخول</div>
+          <div className="input-container">
+            <MdEmail />
+            <Form.Control type="email" placeholder={`البريد الألكتروني`} />
+          </div>
+          <div className="input-container">
+            <FaLock />
+            <Form.Control type="password" placeholder="كلمة السر" />
+          </div>
           <button className="main-btn" type="submit">
             تسجيل الدخول
           </button>
         </div>
-        <div className="img-login"></div>
       </div>
     </section>
   );
--- src/Pages/Authentaction/SignUp/SignUp.jsx
+++ src/Pages/Authentaction/SignUp/SignUp.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [signIn, setSignIn] = useState(true);
+  return (
+    <section className="signUp-section">
+      <div className="container" >
+        <form action="">
+          <title>
+            
+          </title>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default SignUp;
