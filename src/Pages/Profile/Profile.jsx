8aae9a8a95a715c4bcc6eab463cddb558128fd61
AliiMuhammed <mohamedtaha8000@gmail.com>
2024-01-22 04:46:19 +0200

    Refactor profile page UI for displaying user details and adding an edit button
    Updated the profile page to dynamically display the user's details and
    added an edit button for further user interaction. Improved layout and
    styling for a better user experience. No associated issue.

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import "./style/profile.css";
+import { getAuthUser } from "./../../Helpers/Storage";
+import { FaEdit } from "react-icons/fa";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const user = getAuthUser();
+  return (
+    <section className="profile-section">
+      <div className="cover-profile">
+        <div className="container">
+          <div className="profile-detials">
+            <button>
+              <FaEdit />
+            </button>
+            <div
+              className="user-img"
+              style={{ backgroundImage: `url(${user.profileImage})` }}
+            ></div>
+            <h1>{`${user.firstName} ${user.lastName}`}</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
